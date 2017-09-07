5d9a9a59feb22f97fe5d91df5c6b2c9d9bda2475
onigra <3280467rec@gmail.com>
2017-09-07 17:44:21 +0900

    this is it

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'net-ssh'
+gem 'parallel'
+
+group :test do
+  gem 'test-unit'
+end
---
+++ app.rb
@@ -0,0 +1,60 @@
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'sinatra/json'
+require 'net/ssh'
+require 'parallel'
+require './lib/command'
+
+class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::ConfigFile
+  config_file "#{settings.root}/config/config.yml"
+
+  get '/' do
+    redirect '/statuses'
+  end
+
+  get '/statuses' do
+    results = Parallel.map(settings.hosts, in_processes: 5) do |hostname|
+      Net::SSH.start(hostname) do |ssh|
+        ls_stdout = ssh.exec!(Command.deploy_status).split("\n").map { |i| i.split(" ") }
+
+        ls_stdout.inject({}) do |res, item|
+          war = item[2].gsub(".war", "").split('_')
+
+          res[:glassfish_process_running] = !ssh.exec!(Command.glassfish_process).empty?
+          res[war[0]] = {
+            deploy_status: war[1],
+            deploy_timestamp: "#{item[0]} #{item[1]}",
+            health_check: ssh.exec!(Command.health_check war[0]),
+          }
+          res
+        end
+      end
+    end
+
+    response = Hash[[settings.hosts, results].transpose]
+    json response
+  end
+
+  get '/statuses/:hostname' do
+    result = Net::SSH.start(params['hostname']) do |ssh|
+      ls_stdout = ssh.exec!(Command.deploy_status).split("\n").map { |i| i.split(" ") }
+
+      ls_stdout.inject({}) do |res, item|
+        war = item[2].gsub(".war", "").split('_')
+
+        res[:glassfish_process_running] = !ssh.exec!(Command.glassfish_process).empty?
+        res[war[0]] = {
+          deploy_status: war[1],
+          deploy_timestamp: "#{item[0]} #{item[1]}",
+          health_check: ssh.exec!(Command.health_check war[0]),
+        }
+        res
+      end
+    end
+
+    json({ "#{params['hostname']}": result })
+  end
+end
