5df094eadec997b772c6933a5a7d064f0f6f01d9
moremagic <itoumagic@gmail.com>
2017-09-13 19:38:33 +0900

    add tomcat status

--- lib/tomcat/command.rb
+++ lib/tomcat/command.rb
@@ -1,15 +1,16 @@
 module Tomcat
   class Command
     def self.application_list
-      "ls /opt/sms/app"
+      #"ls /opt/sms/app"
+      "ls /opt/apache-tomcat/webapps"
     end
 
-    def self.process_check(app_name)
-      "ps -ef | grep '#{app_name}' | grep -v 'grep'"
+    def self.process_check
+      "ps -ef | grep tomcat | grep -v 'grep'"
     end
 
-    def self.health_check
-      'まだよ'
+    def self.health_check(app_name)
+      "curl -s localhost:8080/#{app_name}/health -o /dev/null -w \"%{http_code}\""
     end
   end
 end
--- lib/tomcat/status.rb
+++ lib/tomcat/status.rb
@@ -4,8 +4,9 @@ module Tomcat
   class Status
     def self.fetch(hostname)
       Net::SSH.start hostname do |ssh|
-        application_list.call(ssh).gsub("biz-", "").split("\n").inject({ hostname: hostname, type: 'tomcat', apps: [] }) do |res, app_name|
-          res[:apps] << { app_name: app_name, process_check: !process_check.call(ssh, app_name).empty? }
+        application_list.call(ssh).gsub("biz-", "").split("\n").inject({ hostname: hostname, type: 'tomcat', status: !process_check.call(ssh).empty? , apps: [] }) do |res, app_name|
+          res[:apps] << { app_name: app_name, health_check: health_check.call(ssh, app_name) }
+          #res[:apps] << { app_name: app_name, health_check: health_check.call(ssh, item[:app_name]) }
           res
         end
       end
@@ -18,7 +19,7 @@ module Tomcat
     end
 
     def self.process_check
-      lambda { |ssh, app_name| ssh.exec!(Command.process_check app_name) }
+      lambda { |ssh| ssh.exec!(Command.process_check) }
     end
 
     def self.health_check
